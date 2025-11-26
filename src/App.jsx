4546b06539b3681523ad10df4ddad8d930bac392
Toha2003 <maxtohir5@gmail.com>
2025-11-26 15:10:37 +0500

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import CounterPage from "./pages/CounterPage";
 import CardPage from "./pages/CardPage";
+import Layout from "./components/layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/counter" element={<CounterPage/>} />
-        <Route path="/card" element={<CardPage/>} />
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<HomePage />} />
+          <Route path="/counter" element={<CounterPage />} />
+          <Route path="/card" element={<CardPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/layout/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container, Nav } from 'react-bootstrap'
+import { NavLink, Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <Container>
+      <Nav className='me-auto'>
+        <NavLink className="nav-link" to="/">Home</NavLink>
+        <NavLink className="nav-link" to="/card">Link</NavLink>
+        <NavLink className="nav-link" to="/counter">Counter</NavLink>
+      </Nav>
+
+      <main>
+        <Outlet/>
+      </main>
+
+      <footer></footer>
+    </Container>
+  )
+}
+
+export default Layout
